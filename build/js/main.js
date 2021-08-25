e27688467f8cf06a20d6875cd5c6f0c70690e217
alex-suschin <alex_suschin@mail.ru>
2021-08-25 12:57:35 +0300

    desk

--- build/js/main.js
+++ build/js/main.js
@@ -1,6 +1,18 @@
 $(function() {
 
+    $('.questions-item-show').click(function() {
+        if ($(this).parents('.questions-item').hasClass('active')) {
+            $(this).parents('.questions-item').removeClass('active');
+            $(this).siblings('.questions-item-hide').hide(200);
+            $('.questions-item-hide').hide(200);
+        } else {
+            $('.questions-item').removeClass('active');
+            $('.questions-item-hide').hide(200);
+            $(this).siblings('.questions-item-hide').show(200);
+            $(this).parents('.questions-item').addClass('active');
+        }
 
+    });
 
 });
 //# sourceMappingURL=../sourcemaps/main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,17 @@
 $(function() {
 
+    $('.questions-item-show').click(function() {
+        if ($(this).parents('.questions-item').hasClass('active')) {
+            $(this).parents('.questions-item').removeClass('active');
+            $(this).siblings('.questions-item-hide').hide(200);
+            $('.questions-item-hide').hide(200);
+        } else {
+            $('.questions-item').removeClass('active');
+            $('.questions-item-hide').hide(200);
+            $(this).siblings('.questions-item-hide').show(200);
+            $(this).parents('.questions-item').addClass('active');
+        }
 
+    });
 
 });
