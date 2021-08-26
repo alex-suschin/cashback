af3185d8004a19eb008a65a43222c48aae0f08aa
alex-suschin <alex_suschin@mail.ru>
2021-08-26 16:03:53 +0300

    fix-client

--- src/js/main.js
+++ src/js/main.js
@@ -18,28 +18,66 @@ $(function() {
 
 });
 
+var flag = false;
 
-$(window).on('load resize', function() {
+size();
 
-    var width = $(window).width();
+$(window).resize(function() {
+    if (flag == false) {
+        size();
+    }
+});
 
-    if (width < '1041') {
+
+function size() {
+    if ($(window).width() < '1041') {
         $('.step-2__right .btn-red').insertAfter($('.step-2__left .circle-text'));
     } else {
         $('.step-2__left .btn-red').insertAfter($('.step-2__right b'));
     }
-    if (width < '991') {
-        $('.bonus-top').click(function() {
-            if ($(this).hasClass('active')) {
-                $(this).siblings('.bonus-wrap').hide(200);
-                $(this).removeClass('active');
+
+    flag = true;
+
+    if ($(window).width() < '1041') {
+        $('.bonus-top-btn-mob').click(function() {
+            if ($(this).parents('.bonus-top').hasClass('active')) {
+                $(this).parents('.bonus-top').siblings('.bonus-wrap').hide(200);
+                $(this).parents('.bonus-top').removeClass('active');
             } else {
                 $('.bonus-top').removeClass('active');
                 $('.bonus-wrap').hide(200);
-                $(this).siblings('.bonus-wrap').show(200);
-                $(this).addClass('active');
+                $(this).parents('.bonus-top').siblings('.bonus-wrap').show(200);
+                $(this).parents('.bonus-top').addClass('active');
             }
         });
     }
 
-});
+    flag = true;
+
+
+}
+
+// $(window).on('load resize', function() {
+
+//     var width = $(window).width();
+
+//     if (width < '1041') {
+//         $('.step-2__right .btn-red').insertAfter($('.step-2__left .circle-text'));
+//     } else {
+//         $('.step-2__left .btn-red').insertAfter($('.step-2__right b'));
+//     }
+//     if (width < '991') {
+//         $('.bonus-top').click(function() {
+//             if ($(this).hasClass('active')) {
+//                 $(this).siblings('.bonus-wrap').hide(200);
+//                 $(this).removeClass('active');
+//             } else {
+//                 $('.bonus-top').removeClass('active');
+//                 $('.bonus-wrap').hide(200);
+//                 $(this).siblings('.bonus-wrap').show(200);
+//                 $(this).addClass('active');
+//             }
+//         });
+//     }
+
+// });
